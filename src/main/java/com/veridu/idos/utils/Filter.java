b00149e6ab3825ff80c8363511989d7a47620066
Cassio M. M. Pereira <cassio@veridu.com>
2016-09-08 14:14:56 -0300

    adding a filter class

---
+++ src/main/java/com/veridu/idos/utils/Filter.java
@@ -0,0 +1,43 @@
+package com.veridu.idos.utils;
+
+import java.util.HashMap;
+
+public class Filter {
+
+    private HashMap<String, String> params = new HashMap<>();
+
+    private Filter() {
+    }
+
+    public Filter addNameFilter(String filter) {
+        return this.addFilterByKeyName("name", filter);
+    }
+
+    public Filter addProviderFilter(String filter) {
+        this.addFilterByKeyName("provider", filter);
+        return this;
+    }
+
+    public Filter addFilterByKeyName(String name, String filter) {
+        this.params.put(name, filter);
+        return this;
+    }
+
+    public static Filter createFilter() {
+        return new Filter();
+    }
+
+    @Override
+    public String toString() {
+        int size = this.params.keySet().size();
+
+        String[] queryElements = new String[size];
+
+        int i = size - 1;
+        for (String key : this.params.keySet())
+            queryElements[i--] = key + "=" + this.params.get(key);
+
+        return String.join("&", queryElements);
+    }
+
+}
